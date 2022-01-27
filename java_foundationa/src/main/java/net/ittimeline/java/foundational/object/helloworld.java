b01667e27ce45f4f458e4788e399e6a91723dbaf
ittimeline <ittimelinedotnet@gmail.com>
2022-01-27 14:19:10 +0800

    我的第一个Java程序

---
+++ java_foundationa/src/main/java/net/ittimeline/java/foundational/object/helloworld.java
@@ -0,0 +1,14 @@
+package net.ittimeline.java.foundational.object;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+/**
+ * @version 我的第一个程序 14:13
+ * @outhor tong 18601767221@163.com
+ * @since JDK11
+ */
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
